48d4411eba2b26691c7e87f34690b000947985fb
mhdhassan99 <mhassan.work99@gmail.com>
2020-08-21 10:42:50 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Restaurant from "./Restaurant";
 
 class Restaurants extends Component {
   render() {
-    return(
+    return (
       <ul>
-        Restaurants Component
+        {this.props.restaurants.map((restaurant) => (
+          <Restaurant
+            key={restaurant.id}
+            changeName={this.props.changeName}
+            restaurant={restaurant}
+            removeRestaurant={this.props.removeRestaurant}
+          />
+        ))}
       </ul>
     );
   }
-};
+}
 
 export default Restaurants;
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,32 @@
-
-import cuid from 'cuid';
+import cuid from "cuid";
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = { restaurants: [] }, action) {
+  switch (action.type) {
+    case "ADD_RESTAURANT":
+      return {
+        ...state,
+        restaurants: [
+          ...state.restaurants,
+          { text: action.payload, id: cuid() },
+        ],
+      };
+    case "REMOVE_RESTAURANT":
+      return {
+        ...state,
+        restaurants: state.restaurants.filter(
+          (restaurant) => restaurant.id !== action.payload
+        ),
+      };
 
+    case "CHANGE_NAME":
+      const restaurant = state.restaurants.find(
+        (restaurant) => restaurant.id === action.payload
+      );
+      restaurant.text = "The Crusty Crab";
+      return { ...state, restaurants: [...state.restaurants] };
+
+    default:
+      return state;
+  }
 }
